26873b655620bd664163a87d8db0138fd12b52df
chiboy <chiboygm1@gmail.com>
2016-08-02 22:57:05 +0800

    ths basic function has worked normal

---
+++ app/controllers/dealUrl.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = function(db){
+	var base = 1000;
+ var collect = db.collection("url");
+ this.shorten = function(req,res){
+	 var url = req.url.replace(/^./,"");
+	// console.log(req);
+   	 collect.findOne({url:url},function(err,result){
+		if(err){throw err;}
+		if(result){
+			res.json({
+						originUrl:result.original_url,
+							shortUrl:result.short_url
+					});
+		}else{
+			var original_url = req.headers.host + req.originalUrl;
+			collect.insert({url:url,original_url:original_url},function(err){
+				if(err){
+					throw err;
+				}
+			});
+			collect.count(function(err,count){
+				if(err){throw err;}
+				var shorten_url = req.headers.host + req.baseUrl+"/"+(count + base);
+				collect.update({url:url},{$set:{short_url:shorten_url}});
+				collect.findOne({url:url},function(err,result){
+					res.json({
+						originUrl:result.original_url,
+							shortUrl:result.short_url
+					});
+				});//将此代码放在count之后，第一次输入地址，没有short×，第二次有？
+				
+			});
+			
+		}
+	 });
+//	console.log(url);
+ };
+  this.origin = function(req,res){
+	  var shorten_url = req.headers.host + req.originalUrl;
+	  console.log(res);
+	  collect.findOne({short_url:shorten_url},function(err,result){
+			if(err){throw err;}
+			res.redirect(result.url);
+	  });
+//	  res.end();
+	};
+};
---
+++ server.js.bak
@@ -0,0 +1,16 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+var shorten = express.Router();
+app.use("/shorten",shorten);
+var client = require("mongodb").MongoClient;
+client.connect("mongodb://localhost:27017/shorten" /*|| "mongodb://chiboy:123010@ds139645.mlab.com:39645/smallworld"*/,function(err,db){
+	if(err) {throw err;}
+	var route = require("./app/routes/route.js");
+	route(shorten,db);
+	app.listen(process.env.PORT || 3000,function(){
+		console.log("listening the port");
+	});
+
+});
