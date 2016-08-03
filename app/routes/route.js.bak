24078ffacf120c81b8e15ea3ff29e52589f1569a
chiboy <chiboygm1@gmail.com>
2016-08-03 09:40:05 +0800

    change the base url

--- app/routes/route.js.bak
+++ app/routes/route.js.bak
@@ -3,6 +3,9 @@
 module.exports = function(rount,db){
 	var dealUrl = require(process.cwd()+"/app/controllers/dealUrl.js");
 	var deal = new dealUrl(db);
+	rount.get("/",function(req,res){
+		res.sendFile(process.cwd()+"/public/index.html");
+	});
 	rount.get(/\/https?:/,deal.shorten)
 	rount.get(/\/\d+/,deal.origin);
 }
--- server.js
+++ server.js
@@ -3,13 +3,12 @@
 var express = require("express");
 var app = express();
 var shorten = express.Router();
-app.use("/shorten",shorten);
 app.use("/public",express.static(process.cwd()+"/public"));
 var client = require("mongodb").MongoClient;
 client.connect(/*"mongodb://localhost:27017/shorten" */ "mongodb://chiboy:123010@ds139645.mlab.com:39645/smallworld",function(err,db){
 	if(err) {throw err;}
 	var route = require("./app/routes/route.js");
-	route(shorten,db);
+	route(app,db);
 	app.listen(process.env.PORT || 3000,function(){
 		console.log("listening the port");
 	});
