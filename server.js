0735a5fa5ceff8e3bc50dfdb3ae6d1bceea7ceee
chiboy <chiboygm1@gmail.com>
2016-08-03 09:36:32 +0800

    try to connect mlab

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var shorten = express.Router();
 app.use("/shorten",shorten);
 app.use("/public",express.static(process.cwd()+"/public"));
 var client = require("mongodb").MongoClient;
-client.connect("mongodb://localhost:27017/shorten" /*|| "mongodb://chiboy:123010@ds139645.mlab.com:39645/smallworld"*/,function(err,db){
+client.connect(/*"mongodb://localhost:27017/shorten" */ "mongodb://chiboy:123010@ds139645.mlab.com:39645/smallworld",function(err,db){
 	if(err) {throw err;}
 	var route = require("./app/routes/route.js");
 	route(shorten,db);
